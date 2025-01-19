24bd9a6bb3421dcb285bb56f947f2794a51dab8f
Emiliano Sgrano <sgrilux@gmail.com>
2025-01-19 19:06:01 +0100

    remove terraform lock

--- core/terraform/modules/k6_reports_web/providers.tf
+++ core/terraform/modules/k6_reports_web/providers.tf
@@ -5,10 +5,5 @@ terraform {
       version               = "5.80.0"
       configuration_aliases = [aws, aws.use1]
     }
-
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 2.0"
-    }
   }
 }
